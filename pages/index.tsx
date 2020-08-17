6efa12223b16ba3f684e7502c00e422bed38af77
Lea Rosema <terabaud@gmail.com>
2020-08-17 15:48:22 +0200

    chore: commit some work

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi, I'm <strong>Lea Rosema</strong>. I am a digital artist and
+          frontend engineer living in Hamburg. You can contact me on{' '}
+          <a href="https://twitter.com/terabaud" target="_blank">
+            Twitter
+          </a>
+          .
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
---
+++ pages/posts/second-post.tsx
@@ -0,0 +1,8 @@
+export default function SecondPost() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <p>This is a test.</p>
+    </main>
+  );
+}
